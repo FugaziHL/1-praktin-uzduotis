aa99aa7ffec40fa6611365a1c85f68160191a603
Neilas Demereckis <s063029@ad.viko.lt>
2026-02-20 11:24:23 +0200

    Surasiau info

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+    const double GBP_Bendras = 0.8729;
+    const double GBP_Pirkti  = 0.8600;
+    const double GBP_Parduoti = 0.9220;
+    const double USD_Bendras = 1.1392;
+    const double USD_Pirkti  = 1.1230;
+    const double USD_Parduoti = 1.1743;
+    const double INR_Bendras = 81.1246;
+    const double INR_Pirkti  = 80.1862;
+    const double INR_Parduoti = 107.8546;
+
+    int pasirinkimas;
+    string valiuta;
+    double kiekis;
+
+    return 0;
+}
